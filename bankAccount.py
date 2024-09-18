73818559dacbdf18f08f97bb2af01336b5bb0ade
CalebFilip <filip0732caleb@gmail.com>
2024-09-18 15:35:32 -0400

    feat: added routing number

--- bankAccount.py
+++ bankAccount.py
@@ -3,11 +3,12 @@ min_balance = 0.01
 
 class BankAccount():
 
-    def __init__(self, customer_name, current_balance, minimum_balance, account_number):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
         self._account_number = account_number
+        self.__routing_number = routing_number
 
     def deposit(self, deposit_amount):
         self.current_balance += deposit_amount
